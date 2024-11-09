4095a10928df19b61a20262a46c0dd759426e0e9
Judah Rand <17158624+judahrand@users.noreply.github.com>
2024-11-09 10:34:05 +0000

    feat(zigbee2mqtt): don't provide default config

--- apps/zigbee2mqtt/entrypoint.sh
+++ apps/zigbee2mqtt/entrypoint.sh
@@ -2,10 +2,4 @@
 set -e
 
 echo "Using '$ZIGBEE2MQTT_DATA' as data directory"
-
-if [ ! -f "$ZIGBEE2MQTT_DATA/configuration.yaml" ]; then
-    echo "Creating configuration file..."
-    cp /configuration.example.yaml "$ZIGBEE2MQTT_DATA/configuration.yaml"
-fi
-
 exec zigbee2mqtt "$@"
